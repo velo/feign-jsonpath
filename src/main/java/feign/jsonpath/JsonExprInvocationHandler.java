06ca3452c7e6fe3e7c1a37d05315f76057e43e17
Marvin Froeder <marvin.froeder@vizexplorer.com>
2016-03-27 20:08:00 +1300

    Added support to java 8 default methods

--- src/main/java/feign/jsonpath/JsonExprInvocationHandler.java
+++ src/main/java/feign/jsonpath/JsonExprInvocationHandler.java
@@ -1,7 +1,11 @@
 package feign.jsonpath;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
 
@@ -27,11 +31,36 @@ public class JsonExprInvocationHandler implements InvocationHandler {
       return type + ": " + document.toString();
     }
 
+    method.setAccessible(true);
+    if (isDefaultMethod(method)) {
+      return invokeJava8DefaultMethod(proxy, method, args);
+    }
+
     JsonExpr path = method.getAnnotation(JsonExpr.class);
-    if(path == null)
+    if (path == null)
       throw new IllegalStateException("Method not annotated with @JsonExpr " + method);
 
     return document.read(path.value());
+  }
+
+  public Object invokeJava8DefaultMethod(Object proxy, Method method, Object[] args) throws NoSuchMethodException,
+      Throwable, IllegalAccessException, InstantiationException, InvocationTargetException {
+    Class<?> declaringClass = method.getDeclaringClass();
+    final Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class
+        .getDeclaredConstructor(Class.class, int.class);
+    if (!constructor.isAccessible()) {
+      constructor.setAccessible(true);
+    }
+
+    return constructor.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
+        .unreflectSpecial(method, declaringClass)
+        .bindTo(proxy)
+        .invokeWithArguments(args);
+  }
+
+  private boolean isDefaultMethod(Method method) {
+    return ((method.getModifiers() & (Modifier.ABSTRACT | Modifier.PUBLIC | Modifier.STATIC)) == Modifier.PUBLIC) &&
+        method.getDeclaringClass().isInterface();
   }
 
   private Object equal(Object[] args) {
@@ -46,6 +75,5 @@ public class JsonExprInvocationHandler implements InvocationHandler {
       return false;
     }
   }
-  
 
 }
